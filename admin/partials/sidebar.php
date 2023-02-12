3f8ce19dfd63281e750781adb65a55b7bae4acd3
Rahul Majumdar <rahulmajumdar400@gmail.com>
2023-02-12 20:47:30 +0530

    Admin Registration Page Updated

--- admin/partials/sidebar.php
+++ admin/partials/sidebar.php
@@ -111,33 +111,14 @@
         <!-- ============================================================= -->
 
 
-
-        
-
-        <li class="nav-heading">Pages</li>
-
-   
-        <li class="nav-item">
-            <a class="nav-link <?php if ($page != 'faq') { echo 'collapsed'; }?>" href="pages-faq.php">
-                <i class="bi bi-question-circle"></i>
-                <span>F.A.Q</span>
-            </a>
-        </li><!-- End F.A.Q Page Nav -->
-
         <li class="nav-item">
             <a class="nav-link collapsed" href="register-admin.php">
                 <i class="bi bi-card-list"></i>
-                <span>Register</span>
+                <span>New Admin</span>
             </a>
         </li><!-- End Register Page Nav -->
 
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="pages-error-404.php">
-                <i class="bi bi-dash-circle"></i>
-                <span>Error 404</span>
-            </a>
-        </li><!-- End Error 404 Page Nav -->
-
+        <!-- <li class="nav-heading">Pages</li> -->
     </ul>
 
 </aside>
--- admin/register-admin.php
+++ admin/register-admin.php
@@ -111,7 +111,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                                         <?php
                                         if ($msg == null) {
 
-                                            echo '<p class="text-center small">Enter your personal details to create account</p>';
+                                            echo '<p class="text-center small">Enter required details to create account</p>';
                                         }else{
                                             echo '<p class="text-center text-danger small">'.$msg.'</p>';
                                         }
@@ -171,7 +171,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                                                 Matched!</div>
                                         </div>
 
-                                        <div class="col-12">
+                                        <!-- <div class="col-12">
                                             <div class="form-check">
                                                 <input class="form-check-input" name="terms" type="checkbox" value=""
                                                     id="acceptTerms" required>
@@ -179,15 +179,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                                                     <a href="#">terms and conditions</a></label>
                                                 <div class="invalid-feedback">You must agree before submitting.</div>
                                             </div>
-                                        </div>
+                                        </div> -->
                                         <div class="col-12">
-                                            <button class="btn btn-primary w-100" name="register" type="submit" id="btn"
-                                                onclick="show()">Create
-                                                Account</button>
-                                        </div>
-                                        <div class="col-12">
-                                            <p class="small mb-0">Already have an account? <a
-                                                    href="pages-login.html">Log in</a></p>
+                                            <button class="btn btn-primary w-100" name="register" type="submit" id="btn" onclick="show()">Create Account</button>
                                         </div>
                                     </form>
 
