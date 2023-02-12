e5ed332cf8ba92009a980dec6d19e6222566a75e
Rahul Majumdar <rahulmajumdar400@gmail.com>
2023-02-12 21:39:40 +0530

    Admin Login Page Title Changed

--- admin/index.php
+++ admin/index.php
@@ -60,14 +60,13 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>Admin Login - Online Devs</title>
+    <title>Admin Login - <?php echo SITE_NAME?></title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
     <!-- Favicons -->
-    <link href="assets/img/favicon.png" rel="icon">
-    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-
+    <link href="<?php echo FAVICON_PATH;?>" rel="icon">
+    <link href="<?php echo APPL_FAV_PATH;?>" rel="apple-touch-icon">
     <!-- Google Fonts -->
     <link
         href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
